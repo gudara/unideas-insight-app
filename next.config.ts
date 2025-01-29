4263a1b88ffe9d9018c8c0d5251f72b2a1c7160e
Udara <jgsucgunawardana@gmail.com>
2025-01-29 17:45:16 +0530

    try ag

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const nextConfig: NextConfig = {
   productionBrowserSourceMaps: false,
   // Use the 'production' environment to enable optimizations
   env: {
-    // Environment variables to access in the app
     DATABASE_URL: process.env.DATABASE_URL || 'your_default_local_url_here'
   },
   compress: true,
