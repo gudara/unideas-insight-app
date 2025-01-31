b109baeb67479cd98f3268117c7865722e05ddfc
Udara <jgsucgunawardana@gmail.com>
2025-01-31 14:23:24 +0530

    change 1

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // reactStrictMode: true, // Enforces strict mode for React
-  // productionBrowserSourceMaps: false,
-  // Use the 'production' environment to enable optimizations
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL || 'your_default_local_url_here'
-  },
-  compress: true,
-  output: "standalone"
 };
+
 
 export default nextConfig;
