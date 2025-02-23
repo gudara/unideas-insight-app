21154cf73eb0a9f0f9726fe1165a792c55880494
Udara <jgsucgunawardana@gmail.com>
2025-02-23 13:25:58 +0530

    update pkg json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-4 items-center sm:items-start">
         <h1 className="text-gray-900 dark:text-white text-7xl tracking-wider decoration-4">Unleash Ideas</h1>
-        <p className="text-gray-700 dark:text-gray-400 px-2 " >The Next generation reporting tool</p>
+        <p className="text-gray-700 dark:text-gray-400 px-2 " >The Next generation reporting tool. join un now</p>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         
